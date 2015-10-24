5f99bff6b653f7ca84ecd780460712da5d6906d7
Antonio Claudio Neiva <antonioclaudioneiva@gmail.com>
2015-10-23 21:29:49 -0300

    Added mouse pointer.

--- sharedvision/src/br/ucsal/screen/ScreenService.java
+++ sharedvision/src/br/ucsal/screen/ScreenService.java
@@ -1,6 +1,10 @@
 package br.ucsal.screen;
 
 import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.MouseInfo;
+import java.awt.PointerInfo;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
@@ -18,13 +22,18 @@ import br.ucsal.util.TimerService;
 public class ScreenService extends Thread {
 
 	private byte[] imageByte;
+	
 	private Robot robot;
+	
 	private Rectangle screenRect;
+	
+	private BufferedImage mouseImage = null;
 
 	public ScreenService() throws AWTException {
 		logger.info("Creating screen service.");
 		createRobot();
 		defineSharedScreenArea();
+		createMouseImage();
 		logger.info("Screen service created.");
 	}
 
@@ -40,6 +49,16 @@ public class ScreenService extends Thread {
 		logger.debug("Shared screen area set.");
 	}
 
+	private void createMouseImage() {
+		logger.debug("Creating cursor image.");
+		mouseImage = new BufferedImage(20, 20, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D graphics = mouseImage.createGraphics();
+		graphics.setColor(Color.BLACK);
+		graphics.drawLine(0, 10, 20, 10);
+		graphics.drawLine(10, 0, 10, 20);
+		logger.debug("Cursor image created.");
+	}
+	
 	@Override
 	public void run() {
 		while (true) {
@@ -51,8 +70,18 @@ public class ScreenService extends Thread {
 	private synchronized void screenCapture() {
 		logger.debug("Capturing screen image...");
 		BufferedImage image = robot.createScreenCapture(screenRect);
+		drawMouse(image);
 		logger.debug("Screen image captured.");
 		convertScreenImageToByteArray(image);
+	}
+
+	private void drawMouse(BufferedImage image) {
+		logger.debug("Drawing mouse image...");
+		PointerInfo pointer = MouseInfo.getPointerInfo();
+		int x = (int) pointer.getLocation().getX();
+		int y = (int) pointer.getLocation().getY();
+		image.getGraphics().drawImage(mouseImage, x, y, null);
+		logger.debug("Mouse image draw.");
 	}
 
 	private void convertScreenImageToByteArray(BufferedImage image) {
