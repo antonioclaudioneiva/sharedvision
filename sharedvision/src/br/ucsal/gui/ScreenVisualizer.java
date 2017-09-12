fd90443c25d1f6018bfb34544f23d7c659b8f465
Claudio Neiva <antonioclaudioneiva@gmail.com>
2017-09-12 09:27:42 -0300

    Ajuste para tela cheia e sempre no topo.

--- sharedvision/src/br/ucsal/gui/ScreenVisualizer.java
+++ sharedvision/src/br/ucsal/gui/ScreenVisualizer.java
@@ -1,7 +1,10 @@
 package br.ucsal.gui;
 
 import java.awt.Graphics;
-import java.awt.Toolkit;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
@@ -9,26 +12,57 @@ import javax.swing.JPanel;
 
 import br.ucsal.properties.PropertiesService;
 
-public class ScreenVisualizer extends JPanel {
+public class ScreenVisualizer extends JPanel implements WindowListener {
 
 	private static final long serialVersionUID = 1L;
 
 	private BufferedImage screenImage;
+	
+	private JFrame jFrame = new JFrame(PropertiesService.getSystemFullIdentification());
 
 	public ScreenVisualizer() {
-		JFrame jFrame = new JFrame(PropertiesService.getSystemFullIdentification());
-//		jFrame.setPreferredSize(new Dimension(400, 300));
-		jFrame.setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
+		jFrame.setLocationRelativeTo(null);
+		jFrame.setUndecorated(true);
 		jFrame.getContentPane().add(this);
+		jFrame.setAlwaysOnTop(true);
+		jFrame.setLocationByPlatform(true);
 		jFrame.pack();
+
+		GraphicsDevice d = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		d.setFullScreenWindow(jFrame);
+
 		jFrame.setVisible(true);
+
+		jFrame.addWindowListener(this);
 	}
+
+	public void windowOpened(WindowEvent event) {
+	};
+
+	public void windowActivated(WindowEvent event) {
+	};
+
+	public void windowDeactivated(WindowEvent event) {
+		jFrame.toFront();
+	}
+
+	public void windowIconified(WindowEvent event) {
+	};
+
+	public void windowDeiconified(WindowEvent event) {
+	};
+
+	public void windowClosed(WindowEvent event) {
+	};
+
+	public void windowClosing(WindowEvent event) {
+	};
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		if (screenImage != null) {
-			g.drawImage(screenImage, 0, 0, null); 
+			g.drawImage(screenImage, 0, 0, null);
 		}
 	}
 
